d7f11e11cad3232b28a1ebe3adf5bbcfcc62055d
Julie Iaccarino <1904063+biscuitWizard@users.noreply.github.com>
2019-10-14 02:54:57 -0700

    Adding program for raspberry pi control code.

--- code/master_controller/program.py
+++ code/master_controller/program.py
@@ -1,13 +1,48 @@
-import main as m
+import time
+import configparser
+import mcu
 
 
 def main():
-    mc = m.MyClass()
+    print("Starting Master Controller...")
 
-    mc.start()
+    # Boot up first by loading the configuration file.
+    print("Loading Configuration...")
+    config = configparser.ConfigParser()
+    # Read the default file.
+    config.read('config.ini')
 
+    # Initialize our modules.
+    print("Loading Modules...")
+    radio = mcu.RadioReader()
+
+    # Apply configuration to relevant modules.
+    framerateMs = (1 / config["DEFAULT"].getint("Framerate", 30)) * 1000
+    radioTimeout = config["DEFAULT"].getfloat("RadioChannelTimeout", 1)
+
+    radio.timeout = radioTimeout
+
+    print("Loading a frame every " + str(framerateMs) + "ms.")
+    print("Radio Channel Timeout: " + str(radioTimeout) + "ms.")
+    # Start the modules.
+    radio.start()
+
+    print("Master Controller successfully started.")
+    # Engage the main program loop.
     while True:
-        mc.loop()
+        # Log starting time.
+        start = time.time()
+
+        # Loop through all modules.
+        radio.loop()
+
+        # Log ending time.
+        durationMs = (time.time() - start) * 1000
+
+        # Sleep.
+        if durationMs >= framerateMs:
+            print("System running late. Allocated " + str(framerateMs) + "ms, took " + str(durationMs) + "ms.")
+        time.sleep(max(0, (framerateMs - durationMs) / 1000))
 
 
 if __name__ == "__main__":
